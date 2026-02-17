4186e4c5a8f05b23e68b14a34353923743031778
ndrew-lau-yng <andrewlaugm@gmail.com>
2026-02-18 00:25:38 +0800

    Creation of inference-service yaml files for k8s and integrating other files to match

--- services/database_service/init.sql
+++ services/database_service/init.sql
@@ -1,16 +1,20 @@
--- Records inputs and outputs from model
-CREATE TABLE IF NOT EXISTS predictions (
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT NOW(),
-    input_json JSONB NOT NULL,            -- Stores input data as JSON
-    prediction_json JSONB NOT NULL        -- Stores prediction result as JSON
-);
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: db-init-sql
+data:
+  init.sql: |
+    CREATE TABLE IF NOT EXISTS predictions (
+      id SERIAL PRIMARY KEY,
+      created_at TIMESTAMP DEFAULT NOW(),
+      input_json JSONB NOT NULL,
+      prediction_json JSONB NOT NULL
+    );
 
---Records metadata about API requests
-CREATE TABLE IF NOT EXISTS request_log(
-    id SERIAL PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT NOW(),
-    route TEXT NOT NULL,                  -- API endpoint accessed
-    status_code INT NOT NULL,             -- HTTP status code of the response
-    latency_ms INT NOT NULL               -- Time taken to process the request in milliseconds
-);
+    CREATE TABLE IF NOT EXISTS request_log (
+      id SERIAL PRIMARY KEY,
+      created_at TIMESTAMP DEFAULT NOW(),
+      route TEXT NOT NULL,
+      status_code INT NOT NULL,
+      latency_ms INT NOT NULL
+    );
